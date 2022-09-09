a8f91c7bac1037304701fe8ed978e5dc52a49d14
Ayush negi <79302271+Light0Yagami@users.noreply.github.com>
2022-09-09 11:03:21 +0530

    Update 03.c

--- 03.c
+++ 03.c
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include <sys/types.h>
 #include<unistd.h>
+#include<stdlib.h>
 
 int main()
 {
@@ -14,10 +15,14 @@ int main()
 	int pid = fork();
 
 	if(pid == 0){
-		sleep(3);
+		sleep(5);
 		printf("Child Process is Running\n");
+		exit(0);
 	}else{
+		
 		printf("Parent Process is Running\n");
 	}
+
+	return 0;
 	
 }
